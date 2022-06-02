4d2ba8429bd5c0eaee9ba85ad406e9068035d8ac
Hayaq1 <hayaq.44@gmail.com>
2022-06-02 10:41:58 +0300

    commit

---
+++ calclute.js
@@ -0,0 +1,22 @@
+// program for a simple calculator
+
+
+
+let calculate;
+
+// using if...else if... else
+if (setOP() == '+') {
+    calculate() = press() + press();
+}
+else if (setOP()  == '-') {
+    calculate() = press() - press();
+}
+else if ( setOP()== '*') {
+    calculate() = press() * press();
+}
+else {
+    calculate() = press() / press() ;
+}
+
+// display the result
+console.log(calculate());
